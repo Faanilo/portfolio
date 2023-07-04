db49cb9e81d0a21536522b7dfedbb02f2912f838
Faanilo <faniloniainaa21@gmail.com>
2023-07-04 22:36:40 +0300

    email modif

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,31 +1,34 @@
 import { Container } from "./styles";
 import emailIcon from "../../assets/email-icon.svg";
-import phoneIcon from "../../assets/phone-icon.svg"
+import phoneIcon from "../../assets/phone-icon.svg";
 import { Form } from "../Form/Form";
 import ScrollAnimation from "react-animate-on-scroll";
 
-
-export function Contact(){
-
-  return(
+export function Contact() {
+  return (
     <Container id="contact">
       <ScrollAnimation animateIn="fadeInRight">
-      <header>
-        <h2>Contact Me</h2>
-        <p>If you saw my potential or want to talk to me, don't hesitate to send me a message.</p>
-      </header>
-      <div className="contacts">
-        <div>
-          <img src={emailIcon} alt="Email" />
-          <a href="mailto:faniloniainaa21@gmail.com">faniloniainaa21@gmail.com</a>
+        <header>
+          <h2>Contact Me</h2>
+          <p>
+            If you saw my potential or want to talk to me, don't hesitate to
+            send me a message.
+          </p>
+        </header>
+        <div className="contacts">
+          <div>
+            <img src={emailIcon} alt="Email" />
+            <a href="mailto:faniloniainaa21@gmail.com">
+              faniloniainaa21@gmail.com
+            </a>
+          </div>
+          <div>
+            <img src={phoneIcon} alt="Phone" />
+            <a href="tel:+261327379128">(+261) 327379128</a>
+          </div>
         </div>
-        <div>
-          <img src={phoneIcon} alt="Email" />
-          <a href="tel:+261327379128">(+261) 327379128</a>
-        </div>  
-      </div>
       </ScrollAnimation>
       <Form></Form>
     </Container>
-  )
+  );
 }
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,22 +1,29 @@
 import { Container } from "./styles";
-import  ScrollAnimation  from "react-animate-on-scroll";
+import ScrollAnimation from "react-animate-on-scroll";
+
 export function Form() {
-  
+  const getformEndpoint = "https://app.getform.io/forms/50246";
+  const recipientEmail = "faniloniainaa21@gmail.com";
+
   return (
     <Container>
       <h2>Get in touch using the form below.</h2>
       <ScrollAnimation animateIn="fadeInLeft">
-      <form
-        action="https://getform.io/f/bec145ac-14cd-445b-bd24-5139a88174bb"
-        method="POST"
-        encType="multipart/form-data"
-      >
-        <input placeholder="Email" id="email" type="email" name="email" />
-        <textarea placeholder="Enter text" id="message" name="message" />
-        <button type="submit">
-          Send
-        </button>
-      </form>
+        <form
+          action={getformEndpoint}
+          method="POST"
+          encType="multipart/form-data"
+        >
+          <input placeholder="Email" id="email" type="email" name="email" />
+          <textarea
+            placeholder="Enter text"
+            id="message"
+            name="message"
+          ></textarea>
+          <input type="hidden" name="_subject" value="New form submission" />
+          <input type="hidden" name="_replyto" value={recipientEmail} />
+          <button type="submit">Send</button>
+        </form>
       </ScrollAnimation>
     </Container>
   );
