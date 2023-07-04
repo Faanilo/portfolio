d3542568f7eb44c3c8c1361eaa26c24d442c0999
Faanilo <faniloniainaa21@gmail.com>
2023-07-04 22:54:28 +0300

    bug email

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,19 +1,34 @@
 import { Container } from "./styles";
 import ScrollAnimation from "react-animate-on-scroll";
+import axios from "axios";
 
 export function Form() {
-  const getformEndpoint = "https://app.getform.io/forms/50246";
-  const recipientEmail = "faniloniainaa21@gmail.com";
+  const getformEndpoint = "https://app.getform.io/forms/50246"; // Update the endpoint URL
+  const recipientEmail = "faniloniainaa21@gmail.com"; // Update with desired recipient email
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    try {
+      await axios.post(getformEndpoint, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      alert("Email submitted successfully!");
+      event.target.reset();
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      alert("Error submitting email. Please try again later.");
+    }
+  };
 
   return (
     <Container>
       <h2>Get in touch using the form below.</h2>
       <ScrollAnimation animateIn="fadeInLeft">
-        <form
-          action={getformEndpoint}
-          method="POST"
-          encType="multipart/form-data"
-        >
+        <form onSubmit={handleSubmit}>
           <input placeholder="Email" id="email" type="email" name="email" />
           <textarea
             placeholder="Enter text"
